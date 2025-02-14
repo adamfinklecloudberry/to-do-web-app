f1733bdbfdd38346f6b538eb4600e77f4ee7ff2c
adamfinklecloudberry <198999961+adamfinklecloudberry@users.noreply.github.com>
2025-02-14 18:32:10 -0500

    Added error handling

--- flask_app.py
+++ flask_app.py
@@ -11,32 +11,48 @@ def home():
 
 @app.route('/add', methods=['POST'])
 def add_task():
-    task = request.form.get('task')
-    due_date = request.form.get('due_date')
-    if task and due_date:
-        tasks.append({'name': task, 'due_date': due_date, 'complete': False})
-        flash('Task added successfully', 'success')
+    try:
+        task = request.form.get('task')
+        due_date = request.form.get('due_date')
+        if task and due_date:
+            tasks.append({'name': task, 'due_date': due_date, 'complete': False})
+            flash('Task added successfully', 'success')
+    except Exception as e:
+        flash('Error in adding task', 'error')
+        print(f"Error when running add_task {e}")
     return redirect('/')
 
 @app.route('/complete/<int:task_id>', methods=['POST'])
 def complete_task(task_id):
-    if task_id < len(tasks):
-        tasks[task_id]['complete'] = not tasks[task_id]['complete']  # Toggle complete status
-        flash('Task completion status updated', 'success')
+    try:
+        if task_id < len(tasks):
+            tasks[task_id]['complete'] = not tasks[task_id]['complete']  # Toggle complete status
+            flash('Task completion status updated', 'success')
+    except Exception as e:
+        flash('Error completing task', 'error')
+        print(f"Error when running complete_task {e}")
     return redirect('/')
 
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
-    if task_id < len(tasks):
-        tasks.pop(task_id)
-        flash('Task deleted successfully', 'danger')
+    try:
+        if task_id < len(tasks):
+            tasks.pop(task_id)
+            flash('Task deleted successfully', 'danger')
+    except Exception as e:
+        flash('Error deleting task', 'error')
+        print(f"Error when running delete_task {e}")
     return redirect('/')
 
 @app.route('/delete_all', methods=['POST'])
 def delete_all_tasks():
-    if tasks:
-        tasks.clear()  # Clear all tasks
-        flash('All tasks deleted successfully', 'danger')
+    try:
+        if tasks:
+            tasks.clear()  # Clear all tasks
+            flash('All tasks deleted successfully', 'danger')
+    except Exception as e:
+        flash('Error deleting all tasks', 'error')
+        print(f"Error when running delete_all_tasks {e}")
     return redirect('/')
 
 if __name__ == '__main__':
