2cf9a86a317202e1bf1185edb6a819de7aca87a9
adamfinklecloudberry <198999961+adamfinklecloudberry@users.noreply.github.com>
2025-02-14 18:56:09 -0500

    Added a method to return all tasks as a json, added docstrings, and improved error message formatting

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import (
+    Flask, 
+    render_template, 
+    request, 
+    redirect, 
+    flash, 
+    jsonify
+)
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -7,10 +14,12 @@ tasks = []
 
 @app.route('/')
 def home():
+    """Returns the home HTML"""
     return render_template('index.html', tasks = tasks)
 
 @app.route('/add', methods=['POST'])
 def add_task():
+    """Adds a task to the task list"""
     try:
         task = request.form.get('task')
         due_date = request.form.get('due_date')
@@ -19,41 +28,52 @@ def add_task():
             flash('Task added successfully', 'success')
     except Exception as e:
         flash('Error in adding task', 'error')
-        print(f"Error when running add_task {e}")
+        print(f"Error when running add_task: {e}")
     return redirect('/')
 
 @app.route('/complete/<int:task_id>', methods=['POST'])
 def complete_task(task_id):
+    """Marks a task as completed"""
     try:
         if task_id < len(tasks):
             tasks[task_id]['complete'] = not tasks[task_id]['complete']  # Toggle complete status
             flash('Task completion status updated', 'success')
     except Exception as e:
         flash('Error completing task', 'error')
-        print(f"Error when running complete_task {e}")
+        print(f"Error when running complete_task: {e}")
     return redirect('/')
 
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
+    """Deletes a task from the list"""
     try:
         if task_id < len(tasks):
             tasks.pop(task_id)
             flash('Task deleted successfully', 'danger')
     except Exception as e:
         flash('Error deleting task', 'error')
-        print(f"Error when running delete_task {e}")
+        print(f"Error when running delete_task: {e}")
     return redirect('/')
 
 @app.route('/delete_all', methods=['POST'])
 def delete_all_tasks():
+    """Deletes all tasks from the list"""
     try:
         if tasks:
             tasks.clear()  # Clear all tasks
             flash('All tasks deleted successfully', 'danger')
     except Exception as e:
         flash('Error deleting all tasks', 'error')
-        print(f"Error when running delete_all_tasks {e}")
+        print(f"Error when running delete_all_tasks: {e}")
     return redirect('/')
+
+@app.route("/api/tasks")
+def get_tasks():
+    """Returns a json containing all the tasks"""
+    try:
+        return jsonify(tasks)
+    except Exception as e:
+        print(f"Error when returning all tasks: {e}")
 
 if __name__ == '__main__':
     app.run()
